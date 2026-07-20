39e4102263abfc873eb8cd94a63a576aece8a5ec
sanjithasridhar <153504630+sanjithasridhar@users.noreply.github.com>
2026-07-20 22:19:09 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int count =0;
+        for (int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+        if(count>=1)
+            {
+                return true;
+            }
+        else {
+            return false;
+        }
+
+     }   
+   
+ }
